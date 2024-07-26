6d3c02b96016e062439df6f826c7b5b78f30fb28
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-26 10:00:52 +0200

    Aggiungere contenuti lorem

--- resources/views/component/main.blade.php
+++ resources/views/component/main.blade.php
@@ -7,9 +7,9 @@
     <section>
         <article>
             <h2>Titolo</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores amet fugit id, mollitia libero maxime omnis repudiandae quaerat similique in reiciendis quia quam aut incidunt, debitis ipsa, labore temporibus ratione?
-            Aperiam, suscipit sed. Iusto quae dolorem voluptas. Ratione magni quasi aliquid ex reiciendis doloremque soluta dolorem id laudantium doloribus quam, nulla esse optio sint repellat repudiandae ducimus. Blanditiis, cumque nobis?
-            Mollitia atque quis impedit provident veniam quo. Nisi adipisci perferendis perspiciatis tempore, cupiditate reiciendis consequuntur dolorum iusto ullam incidunt totam nihil unde quaerat assumenda, sequi, consectetur sit ipsum possimus sunt!</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores amet fugit id, mollitia libero maxime omnis repudiandae quaerat similique in reiciendis quia quam aut incidunt, debitis ipsa, labore temporibus ratione? Aperiam, suscipit sed. Iusto quae dolorem voluptas. Ratione magni quasi aliquid ex reiciendis doloremque soluta dolorem id laudantium doloribus quam, nulla esse optio sint repellat repudiandae ducimus. Blanditiis, cumque nobis? Mollitia atque quis impedit provident veniam quo. Nisi adipisci perferendis perspiciatis tempore, cupiditate reiciendis consequuntur dolorum iusto ullam incidunt totam nihil unde quaerat assumenda, sequi, consectetur sit ipsum possimus sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero voluptate quidem porro ipsum id temporibus quaerat, veritatis consectetur aspernatur earum consequatur blanditiis dolor ab beatae corporis. Cupiditate id odit doloribus.Sequi, illo sapiente debitis quas tempore, harum voluptatum cumque, dolores tempora incidunt beatae magnam maxime asperiores natus minima repellat iure dolorem placeat voluptates neque? Nobis veritatis quas aspernatur vero id?
+            </p>
         </article>
     </section>
 </main>
