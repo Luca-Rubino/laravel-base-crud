43053707e9ea9ea3201ad0ab01a8e1e8e806c19a
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-26 10:00:00 +0200

    Formatazione index

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/scss/app.scss', 'resources/js/app.js')
     <title>@yield('title-page', 'Index')</title>
 </head>
+
 <body>
     @include('component.nav')
     @include('component.main')
@@ -13,4 +15,5 @@
 
     {{-- @include('resources.js.myScript') --}}
 </body>
+
 </html>
