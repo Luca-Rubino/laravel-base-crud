e310458f08deaff8f95376f91bc581ce21920f59
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-26 10:00:31 +0200

    inserire liste in footer

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -25,5 +25,57 @@
                 </li>
             </ul>
         </article>
+
+        <article>
+            <ul>
+                <li>
+                    <h2>Titolo</h2>
+                </li>
+                <li>
+                    <p>item 1</p>
+                </li>
+                <li>
+                    <p>item 2</p>
+                </li>
+                <li>
+                    <p>item 3</p>
+                </li>
+                <li>
+                    <p>item 4</p>
+                </li>
+                <li>
+                    <p>item 5</p>
+                </li>
+                <li>
+                    <p>item 6</p>
+                </li>
+            </ul>
+        </article>
+
+        <article>
+            <ul>
+                <li>
+                    <h2>Titolo</h2>
+                </li>
+                <li>
+                    <p>item 1</p>
+                </li>
+                <li>
+                    <p>item 2</p>
+                </li>
+                <li>
+                    <p>item 3</p>
+                </li>
+                <li>
+                    <p>item 4</p>
+                </li>
+                <li>
+                    <p>item 5</p>
+                </li>
+                <li>
+                    <p>item 6</p>
+                </li>
+            </ul>
+        </article>
     </section>
 </footer>
